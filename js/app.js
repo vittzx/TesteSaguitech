9d61de05bcadc6af8c24065b0344b9d86284e8d1
Vitor Cosso <vitorcosso@gmail.com>
2023-11-30 18:07:31 -0300

    feat: adicionando data e hora

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 function calcularNotaFiscal(){
+    const empresa = document.getElementById('empresa').value;
     const cliente = document.getElementById('cliente').value;
     const servico = document.getElementById('servico').value;
     const valor = parseFloat(document.getElementById('valor').value);
@@ -9,14 +10,19 @@ function calcularNotaFiscal(){
 
     const tbody = document.querySelector('#tbody');
 
+    let data = new Date();
+
     tbody.innerHTML += `
     
     <tr>
+        <td class="pad">${empresa}</td>
         <td class="pad">${cliente}</td>
         <td class="pad">${servico}</td>
         <td class="pad">${valor.toFixed(2)}</td>
         <td class="pad">${imposto.toFixed(2)}</td>
         <td class="pad">${total.toFixed(2)}</td>
+        <td class="pad">${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}</td>
+        <td class="pad">${data.getUTCHours()-3}:${data.getMinutes()}</td>
     </tr>
     `
 }
